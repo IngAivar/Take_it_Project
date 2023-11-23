515b95bd4db228a3213857239b1b9ab1c92ba404
IngAivar <MiniGan067@yandex.ru>
2023-11-23 15:32:58 +0300

    model.Item added and admin

--- web_shop_app/models.py
+++ web_shop_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    """
+        Django Модель Item с полями (name, description, price)
+    """
+
+    id = models.IntegerField(primary_key=True, serialize=True, unique=True)
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=256)
+    price = models.FloatField(default=1.00)
+
+    def __str__(self):
+        return self.name
