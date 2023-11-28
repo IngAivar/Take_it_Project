d2b2680af013474564b66818b8e89ae6946b674c
IngAivar <MiniGan067@yandex.ru>
2023-11-28 17:43:48 +0300

    continue stripe

--- web_shop_app/urls.py
+++ web_shop_app/urls.py
@@ -4,5 +4,6 @@ from web_shop_app import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("item/<int:id>/", views.detail, name="detail"),
+    # path("item/<int:id>/", views.detail, name="detail"),
+    path("item/", views.item, name="item"),
 ]
--- web_shop_app/views.py
+++ web_shop_app/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import stripe
 
+from config import settings
 from .models import Item
+
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 def index(request):
@@ -12,3 +20,24 @@ def detail(request, id):
     i_list = Item.objects.filter(id=id)
     context = {"id": id, "item_list": i_list}
     return render(request, "web_shop_app/item_preview.html", context)
+
+
+@app.route('/item', methods=['POST'])
+def item():
+  session = stripe.checkout.Session.create(
+    line_items=[{
+      'price_data': {
+        'currency': 'usd',
+        'product_data': {
+          'name': 'T-shirt',
+        },
+        'unit_amount': 2000,
+      },
+      'quantity': 1,
+    }],
+    mode='payment',
+    success_url='http://localhost:8000/success',
+    cancel_url='http://localhost:8000/cancel',
+  )
+
+  return redirect(session.url, code=303)
